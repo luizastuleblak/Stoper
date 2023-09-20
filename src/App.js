8af38ae02c48f13159d15fa377f22c4a70df8a7a
Luiza0607 <110295222+Luiza0607@users.noreply.github.com>
2023-09-20 11:01:33 +0200

    Upadated package

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Container from './components/Container/Container';
+import Timer from './components/Timer/Timer'
+
+
+const App = () => {
+	return (
+		<div>
+			<Container />
+			<Timer />
+			<Container/>
+		</div>
+	);
+};
+
+export default App;
---
+++ src/components/Timer/Timer.js
@@ -0,0 +1,43 @@
+import Button from '../Buttons/Button';
+import FormattedTime from '../FormatedTime/FormatedTime';
+import { useState, useEffect } from 'react';
+
+const Timer = () => {
+	const [time, setTime] = useState(0);
+	const [timer, setTimer] = useState('');
+
+	const start = () => {
+		setTimer(
+			setInterval(() => {
+				setTime((prevValue) => prevValue + 1);
+			}, 1)
+		);
+	};
+
+	useEffect(() => {
+		return () => {
+			if (timer) clearInterval(timer);
+		};
+	}, [timer]);
+
+	const handleStart = () => {
+		start();
+	};
+	const handleStop = () => {
+		clearInterval(timer);
+	};
+	const handleReset = () => {
+		setTime(0);
+	};
+
+	return (
+		<div>
+			<FormattedTime time={time} />
+			<Button action={handleStart}>Start</Button>
+			<Button action={handleStop}>Stop</Button>
+			<Button action={handleReset}>Reset</Button>
+		</div>
+	);
+};
+
+export default Timer;
